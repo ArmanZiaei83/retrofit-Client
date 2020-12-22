de3092ab6bf83781818907b49b5a8158fb0636b6
armanziaei <armanziaei83@gmaio.com>
2020-12-22 18:54:39 +0330

    Feat Post.java File : Post.java file created for retrofit process./

---
+++ app/src/main/java/com/example/ohhttp/Post.java
@@ -0,0 +1,30 @@
+package com.example.ohhttp;
+
+public class Post {
+    private int userId;
+    private Integer id;
+    private String title;
+    private String body;
+
+    public Post(int userId, String title, String body) {
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
